4e3e1bf74eef6c261d078da4550b902c3758b9c9
Lipis <lipiridis@gmail.com>
2020-03-31 13:52:46 +0300

    Initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { Link } from "gatsby";
+import { ThemeToggler } from "gatsby-plugin-dark-mode";
+import Toggle from "./Toggle";
+import sun from "../assets/sun.png";
+import moon from "../assets/moon.png";
+import { rhythm, scale } from "../utils/typography";
+import "./layoutStyles.css";
+import Excalidraw from "./excalidraw";
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`;
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(26),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      <div
+        style={{
+          position: "fixed",
+          right: "8px",
+          top: "8px",
+        }}
+      >
+        <ThemeToggler>
+          {({ theme, toggleTheme }) => {
+            return (
+              <Toggle
+                icons={{
+                  checked: (
+                    <img
+                      src={moon}
+                      width="16"
+                      height="16"
+                      alt="presentation"
+                      style={{ pointerEvents: "none" }}
+                    />
+                  ),
+                  unchecked: (
+                    <img
+                      src={sun}
+                      width="16"
+                      height="16"
+                      alt="presentation"
+                      style={{ pointerEvents: "none" }}
+                    />
+                  ),
+                }}
+                checked={theme === "dark"}
+                onChange={() => {
+                  toggleTheme(theme === "light" ? "dark" : "light");
+                }}
+              />
+            );
+          }}
+        </ThemeToggler>
+      </div>
+      <header>
+        <div style={{ fontFamily: "var(--ui-font)", textAlign: "right" }}>
+          {location.pathname !== rootPath && (
+            <span style={{ float: "left" }}>
+              <Link to="/">All posts</Link>
+            </span>
+          )}
+          <Excalidraw />
+        </div>
+        <span style={{ clear: "both" }} />
+        {location.pathname === rootPath ? (
+          <h1
+            style={{
+              ...scale(1.2),
+              marginTop: 0,
+            }}
+          >
+            {title}
+          </h1>
+        ) : null}
+      </header>
+      <main>{children}</main>
+      <footer
+        style={{
+          textAlign: "center",
+          padding: `${rhythm(2)} 0`,
+        }}
+      >
+        <span>
+          © {new Date().getFullYear()} Excalidraw
+          {" • "}
+          <a href="https://github.com/excalidraw/excalidraw-blog/blob/master/LICENSE">
+            MIT Licensed
+          </a>
+          {" • "}
+          <a href="https://twitter.com/excalidraw">Twitter</a>
+          {" • "}
+          <a href="https://github.com/excalidraw/excalidraw-blog">
+            View source
+          </a>
+          {" • "}
+          <Link to="/">All posts</Link>
+        </span>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { rhythm } from "../utils/typography";
+
+function BlogIndex({ data, location }) {
+  const posts = data.allMarkdownRemark.edges;
+  const title = data.site.siteMetadata.title;
+
+  return (
+    <Layout location={location} title={title}>
+      <SEO title="All posts" />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug;
+        return (
+          <div key={node.fields.slug}>
+            <h3
+              style={{
+                marginBottom: rhythm(1 / 4),
+              }}
+            >
+              <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                {title}
+              </Link>
+            </h3>
+            <p>
+              <strong>{node.frontmatter.date}</strong>
+              {" — "}
+              <span style={{ opacity: 0.75 }}>{node.excerpt}</span>
+            </p>
+          </div>
+        );
+      })}
+    </Layout>
+  );
+}
+
+export default BlogIndex;
+
+export const pageQuery = graphql`
+  query BlogIndex {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`;
